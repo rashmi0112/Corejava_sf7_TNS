556d909eb96a8e55470c7c73a02064a3d455dcb6
Umashankar <Umashankar@LAPTOP-J8FVUM2C>
2026-01-24 12:23:28 +0530

    commit

--- Corejavaprogram/src/basicprogram/Dm1.java
+++ Corejavaprogram/src/basicprogram/Dm1.java
@@ -1,10 +1,18 @@
 package basicprogram;
 
 public class Dm1 {
-
+	
 	public static void main(String[] args) {
+		int $name;
+	    //int a=2;//
+	    float b=70.6f;//data widening//
+	    int y=(int)b;//data narrowing//
+
 		System.out.println("welcome");
-		// TODO Auto-generated method stub
+		//System.out.println(a);//
+		System.out.println(b);
+		System.out.println(y);
+		
 
 	}
 
---
+++ Corejavaprogram/src/basicprogram/gh.java
@@ -0,0 +1,10 @@
+package basicprogram;
+
+public class gh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
